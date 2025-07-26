9c5e5477ffaf09e5a8d3c5d95f708596dba7abc3
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2025-07-26 13:15:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-<<<<<<< HEAD
 resorce "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
-=======
-resource "aws_instance" "example" {
- ami = var.ami_id
- instance_type = var.instance_type
->>>>>>> 77a6942e69253edb0bfd1236e389cffa83413320
-
   tags = {
 
     Name = "Terraform-Github-Instance"
